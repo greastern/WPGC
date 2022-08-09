b03db074d746b162cc16f5a9e301213c49ad6511
Gaius Roy <gaiusroy@gmail.com>
2022-08-09 01:28:42 -0400

    Made background animated with scroll

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-// imports
-import gsap from "gsap";
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
 
+// END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
 
-// get other plugins:
-import ScrollTrigger from "gsap/ScrollTrigger";
-import Flip from "gsap/Flip";
-import Draggable from "gsap/Draggable";
+// scrollmagic
+const controller = new ScrollMagic.Controller();
 
-// or all tools are exported from the "all" file (excluding members-only plugins):
-import { gsap, ScrollTrigger, Draggable, MotionPathPlugin } from "gsap/all";
+// Scenes
+const scene = new ScrollMagic.Scene({
+    duration: 60000,
+    triggerElement: intro,
+    triggerHook: 0
+})
+    .addIndicators()
+    .setPin(intro)
+    .addTo(controller);
 
-// don't forget to register plugins
-gsap.registerPlugin(ScrollTrigger, Draggable, Flip, MotionPathPlugin); 
+    // Video Animations
+    let accelamount = .1;
+    let scrollpos = 0;
+    let delay = 0;
+
+    scene.on("update", e => {
+        scrollpos = e.scrollPos / 1000; // e.scrollPos => is coming from scrollmagic on the event
+        // console.log(e) // logs position
+    })
+
+    setInterval(() => {
+        delay += (scrollpos - delay) * accelamount;
+        console.log(scrollpos, delay);
+
+        video.currentTime = delay
+    }, 41.6); // 33.3 = framerate 
